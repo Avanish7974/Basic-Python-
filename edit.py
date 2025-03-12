4cb2666e8a76e9d4e240a6398c5db031d4cffb7b
Avanish7974 <avanishsingh4318@gmail.com>
2025-03-12 18:53:05 +0530

    tri

--- edit.py
+++ edit.py
@@ -124,3 +124,14 @@ for i in x:
 # WAP using function wih all 4 type. Also use while loop and for loop to make a choice based of 20 program in which 10 are from patterns and 10 are from armstron,lcm,hcf,prime etc.
     
 # note : user can exit from the program only on press 21    
+# select salary from c1 inner join c2 on c1.c_id>c2.c_id;
+# row draw by i and x axis
+# column draw by j and y axis
+
+# to print square
+for i in range(1,6):
+    for j in range(1,6):
+        print(i,end=" ")
+    print()    
+
+# to print left triangle    
