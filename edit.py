5cb9b270a578d036e81b76c254ca4b067fa7b9ea
Avanish7974 <avanishsingh4318@gmail.com>
2025-03-12 18:40:50 +0530

    factor

--- edit.py
+++ edit.py
@@ -5,4 +5,19 @@ ch=eval(input("Enter Your choice : "))
 print(ch)
 # 10*20
 print("hlo")
-# er
+k = int(input("Enter the Starting number : "))
+j = int(input("Enter the ending number : "))
+if k>0:
+    for i in range(k,j+1):
+        cnt = 0
+        for j in range(1,i+1):
+            if(i%j==0):
+                cnt+=1
+        if(cnt==2):
+            print("prime number are ",i) 
+        elif(cnt==4):
+            print("4 factors are ",i)           
+
+
+    
+
