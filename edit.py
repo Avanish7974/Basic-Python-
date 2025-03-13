f0e8833b35597af7893b97c2a8f2ed9827dd5474
Avanish7974 <avanishsingh4318@gmail.com>
2025-03-13 22:33:54 +0530

    mess

--- edit.py
+++ edit.py
@@ -326,8 +326,3 @@ for i in range(7):
 
 #     for j in range(5):
 #         if( i== 3 or j==2 or (j==4 and i>=3) or   (j==0 and i<=3) or (i==0 and j>=2) or (i==6 and j<=2) or (i==1 and j<=2) ):
-#             print("*",end=" ")
-#         else:
-#             print(" ",end=" ")
-#     print()    
-    
