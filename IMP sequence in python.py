6e50dfe78e56a517d33e60f3a4d173ce05f081fb
Avanish7974 <avanishsingh4318@gmail.com>
2025-03-20 19:34:34 +0530

    list

---
+++ IMP sequence in python.py
@@ -0,0 +1,9 @@
+# Sequence in Python
+# # LIST
+# 1. LIST - When We Want To Represent A group Of Individual Entity In a Single Entity Then We Use List 
+# 2. In list we can store a hitrogeneous as well as homogeneous types of data
+# 3. List are muttable(changes are allowed)
+
+# 4. Representation of list mylist = [1,2,34,5,'A','B','C','D']
+# 5.  In list position are specified which is known as Index,from start to end of an list 0,1,2,3 and so   on and from end to start from -1,-2,-3 and so on 
+
