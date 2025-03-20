b80c1df52af2ff875fc71c65b84bf4afbc332a87
Avanish7974 <avanishsingh4318@gmail.com>
2025-03-20 19:15:43 +0530

    lambda 2

--- lambda function.py
+++ lambda function.py
@@ -1,2 +1,13 @@
-lambda function is a one line function and it is also called annonomus function in python 
-annonomus function is beenaam function
+# lambda function is a one line function and it is also called annonomus function in python 
+# annonomus function is beenaam function
+
+
+
+# NOTE : Lambda function is a return type function 
+
+x = lambda y:y*12
+print(x)
+# this program return this  0x0000017FD08BCA40> whic is 
+
+x = lambda y:y*12
+print(x(12))
