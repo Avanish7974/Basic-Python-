a57cb32c1d14da4e688316a6a3a2275a23160cac
Avanish7974 <avanishsingh4318@gmail.com>
2025-03-22 00:25:28 +0530

    life

--- List/2 largest.py
+++ List/2 largest.py
@@ -7,9 +7,6 @@ for i in range(n):
     if(list[i]>max):
         max2 = max
         max = list[i]
-    
-        
-            
-            
+
 print("The maximum value in array is : " ,max)  
 print("The Second maximum value in array is : " ,max2)
--- List/Sorting.py
+++ List/Sorting.py
@@ -4,9 +4,9 @@ n = len(list)
 for i in range(n):
     for j in range(i+1,n):
         if(list[i]>list[j]):
-            t = list[i]
+            temp = list[i]
             list[i] = list[j]
-            list[j] = t
+            list[j] = temp
 print(list)  
 
 
