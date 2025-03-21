cce66f37436e9b46715719cc324e76a4292dc332
Avanish7974 <avanishsingh4318@gmail.com>
2025-03-22 00:18:38 +0530

    second

--- List/2 largest.py
+++ List/2 largest.py
@@ -1,1 +1,15 @@
-list = [10,20,30,44,1]
+print("Print Second max value ")
+list= [40,20,30,78,23,99]
+n = len(list)
+max = 0
+max2 = 0
+for i in range(n):
+    if(list[i]>max):
+        max2 = max
+        max = list[i]
+    
+        
+            
+            
+print("The maximum value in array is : " ,max)  
+print("The Second maximum value in array is : " ,max2)
