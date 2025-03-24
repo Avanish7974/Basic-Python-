e1202d7a8c8c93ecf67ed393bce5a5b0ca9bddfe
Avanish7974 <avanishsingh4318@gmail.com>
2025-03-24 19:11:10 +0530

    tup001

---
+++ tuple.py
@@ -0,0 +1,3 @@
+# tuple
+# When we wan to represent a group of individual object into an single entity where duplicate and homgeneous and hetrogeneous object is to be allowed and insertion order is to be preserved but we want immutable behaviour of the object then we will use a tuple
+
