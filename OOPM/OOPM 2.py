73118649bc55520ec2d610d62b9c335cf9128d24
Avanish7974 <avanishsingh4318@gmail.com>
2025-03-31 19:16:19 +0530

    college

---
+++ OOPM/OOPM 2.py
@@ -0,0 +1,10 @@
+class college:
+    def course():
+        print("Our courses")
+    def coursefees():
+        print("Coursefees is = 454354")
+    def duration():
+        print("duration of 1")
+obj = college
+obj.duration() 
+obj.course()            
