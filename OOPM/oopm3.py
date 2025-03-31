00d5fb86874758ad7d4b3365182b0e73aea45681
Avanish7974 <avanishsingh4318@gmail.com>
2025-03-31 19:59:57 +0530

    enter

--- OOPM/oopm3.py
+++ OOPM/oopm3.py
@@ -4,7 +4,7 @@ class myclass:
         self.lnm = lnm
     def show(self):    
 
-        print("Hllo display",self.fnm,self.lnm)
+        print("Bank Balance : ",self.fnm,self.lnm)
 ob = myclass()
 n1 = int(input("Enter first number :"))  
 n2 = int(input("Enter Second number :"))          
@@ -12,3 +12,5 @@ ob.display(n1,n2)
 ob.show()
 
 # WAP to make calculator using self and return type with argument function input taken frm user
+
+# wap to make bank management system on press 1 see the account details on press2 edit account balance on press 3 eddit personal details 
