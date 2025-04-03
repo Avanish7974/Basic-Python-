3017afe837388bf01e5c859b71e4b2563c1b69d9
Avanish7974 <avanishsingh4318@gmail.com>
2025-04-03 19:22:51 +0530

    rbi

---
+++ OOPM/multiple.py
@@ -0,0 +1,7 @@
+# when a one class inherit the property of more than one parent class is called multiple polymorphism
+
+class sbi:
+    def sbiroi(self,amt):
+        self.amt=amt
+        
+        
