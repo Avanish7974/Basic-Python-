63a1c18c5e34a07cf8873385e1c13a00a9e227b1
Avanish7974 <avanishsingh4318@gmail.com>
2025-04-03 19:28:30 +0530

    hdfc

--- OOPM/multiple.py
+++ OOPM/multiple.py
@@ -3,5 +3,20 @@
 class sbi:
     def sbiroi(self,amt):
         self.amt=amt
-        
+        self.ir=self.ir*7.8/100
+        print("SBI ROI = ",self.ir)
+class icici:
+    def iciciroi(self,amt):
+        self.amt=amt
+        self.ir=self.ir*9.8/100
+        print("icici ROI = ",self.ir)
+class hdfc:
+    def hdfcroi(self,amt):
+        self.amt=amt
+        self.ir=self.ir*12.8/100
+        print("hdfc ROI = ",self.ir)                
+class BANK(sbi,hdfc,icici):
+    pass
+obj=BANK()
+
         
