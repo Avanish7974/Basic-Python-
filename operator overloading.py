88230933c8b3c44d3487584f07c7a1c5e8e13c77
Avanish7974 <avanishsingh4318@gmail.com>
2025-04-04 20:04:35 +0530

    default

---
+++ operator overloading.py
@@ -0,0 +1,21 @@
+# when we perform addition,subtraction,multiplication and so on ,on a object of an class we can overload the operator using magic method 
+
+
+class class1:
+    def __init__(self,PARA):
+        self.para=PARA
+    def __mul__(self,other):
+        return self.para*other.para
+ob1 = class1(100)
+ob2 = class1(200)
+print(ob1*ob2)   
+
+
+class mycls:
+    def func1(self,para):
+        self.para=para
+    def __add__(self,other):
+        return self.para + self.other
+ob = mycls(100)
+print(ob)        
+    
