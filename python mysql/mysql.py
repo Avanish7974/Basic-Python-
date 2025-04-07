182b5fdf6782d38652c307acfca1f9ae2e3096d0
Avanish7974 <avanishsingh4318@gmail.com>
2025-04-07 19:23:21 +0530

    pip

---
+++ python mysql/mysql.py
@@ -0,0 +1,9 @@
+
+# python can be used in data base 
+# MY sql is rdbms software in which we can create database and one database can have multiple tables 
+
+# how to install mysql drivers in python
+
+# python needs a mysql driver to access the my sql data base 
+# pip(python installer packages) - 
+
