0d4f61a01df6b66fe41e29cb93f277c8319e2c40
Avanish7974 <avanishsingh4318@gmail.com>
2025-04-09 20:14:57 +0530

    impot

---
+++ python mysql/import.py
@@ -0,0 +1,9 @@
+import sql002
+
+obj1 = sql002.dbconn()
+ch = int(input("Enter your choice : "))
+if(ch==1):
+    rollno=input("Enter roll:")
+    name = input("Enter name ")
+    address = input("Enter address ")
+    class1 = input("Enter class : ")
--- python mysql/sql002.py
+++ python mysql/sql002.py
@@ -7,3 +7,14 @@ class dbconn:
             password="0000",
             database="python"
         )
+        self.mycursor=self.mydb.cursor()
+    def recordinsert(self,rollno,name,address,class1):
+        self.rollno = rollno
+        self.name = name
+        self.address = address
+        self.class1 = class1
+        sql="insert into data2 (rollno,name,address,class1) values(%s,%s,%s,%s)"    
+        value = [self.rollno,self.name,self.address,self.class1]
+        self.mycursor.execute(sql,value)
+        self.mydb.commit()
+        print(" Successfully added")
